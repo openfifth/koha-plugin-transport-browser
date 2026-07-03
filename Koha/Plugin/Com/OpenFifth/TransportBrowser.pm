8445a0be5a961c5a923b095e38175fcafdaf03fe
Martin Renvoize <martin.renvoize@openfifth.co.uk>
2026-07-03 19:12:01 +0100

    chore: bump version

--- Koha/Plugin/Com/OpenFifth/TransportBrowser.pm
+++ Koha/Plugin/Com/OpenFifth/TransportBrowser.pm
@@ -25,7 +25,7 @@ use Try::Tiny qw(catch try);
 use Koha::File::Transports;
 use Koha::Logger;
 
-our $VERSION = '1.0.6';
+our $VERSION = '1.0.7';
 
 our $metadata = {
     name            => 'Transport Browser',
